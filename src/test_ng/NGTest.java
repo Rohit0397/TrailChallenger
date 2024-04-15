082d1a98cacdcb2b37ab238e25c89296ee131173
Rohit0397 <rohit030897@gmail.com>
2024-04-15 12:09:11 +0530

    Login complete

--- src/test_ng/NGTest.java
+++ src/test_ng/NGTest.java
@@ -1,7 +1,9 @@
 package test_ng;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
@@ -14,14 +16,31 @@ public class NGTest {
         WebDriver driver = new ChromeDriver();
 
         // Navigate to Google
-        driver.get("https://www.google.com");
+        driver.get("http://dev.trailchallenger.support/");
 
         // Perform Selenium actions (e.g., interact with web elements, assert results)
         
-        Thread.sleep(6000);
+        Thread.sleep(5000);
     	driver.manage().window().maximize();
     	
     	// Comment
+    	Thread.sleep(3000);
+    	
+    	// Get the WebElement corresponding to the Email Address(TextField)		
+        WebElement emailField = driver.findElement(By.xpath("//input[@autocomplete=\"username\"]"));		
+        emailField.sendKeys("trailchallenger@yopmail.com");
+        
+        Thread.sleep(3000);
+
+        // Get the WebElement corresponding to the Password Field		
+        WebElement passwordField = driver.findElement(By.xpath("//input[@formcontrolname=\"password\"]"));							
+        passwordField.sendKeys("12345678");
+        
+        Thread.sleep(2000);
+        
+        // Clicking on Login button to Login the panel
+        WebElement loginButton = driver.findElement(By.xpath("//button[@class='btn btn-secondary px-4']"));
+        loginButton.click();
 
       
     }
